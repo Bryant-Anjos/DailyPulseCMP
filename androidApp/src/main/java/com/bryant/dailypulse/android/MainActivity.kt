2006d97f5853a242d5900861c1e1dd05bbceb020
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2024-02-24 20:53:38 -0300

    create the about screen for android

--- androidApp/src/main/java/com/bryant/dailypulse/android/MainActivity.kt
+++ androidApp/src/main/java/com/bryant/dailypulse/android/MainActivity.kt
@@ -8,36 +8,28 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.bryant.dailypulse.Platform
+import com.bryant.dailypulse.android.screens.AboutScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        Platform().logSystemInfo()
-
         setContent {
             MyApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView("Hello, Android")
+                    AboutScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        AboutScreen()
     }
 }
