4dc0c2fa13eea4118a89ca6e5c0c46b909d812ac
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2024-02-25 22:15:22 -0300

    create the android articles screen

--- androidApp/src/main/java/com/bryant/dailypulse/android/MainActivity.kt
+++ androidApp/src/main/java/com/bryant/dailypulse/android/MainActivity.kt
@@ -3,23 +3,28 @@ package com.bryant.dailypulse.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.bryant.dailypulse.android.screens.AboutScreen
+import com.bryant.dailypulse.android.screens.ArticlesScreen
+import com.bryant.dailypulse.articles.ArticlesViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val articlesViewModel: ArticlesViewModel by viewModels()
+
         setContent {
             MyApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AboutScreen()
+                    ArticlesScreen(articlesViewModel = articlesViewModel)
                 }
             }
         }
--- shared/src/commonMain/kotlin/com/bryant/dailypulse/articles/ArticlesViewModel.kt
+++ shared/src/commonMain/kotlin/com/bryant/dailypulse/articles/ArticlesViewModel.kt
@@ -17,7 +17,9 @@ class ArticlesViewModel: BaseViewModel() {
 
     private fun getArticles() {
         scope.launch {
-            delay(500)
+            delay(2000)
+            _articlesState.emit(ArticlesState(error = "Something went wrong"))
+            delay(2000)
             val fetchedArticles = fetchArticles()
             _articlesState.emit(ArticlesState(articles = fetchedArticles))
         }
