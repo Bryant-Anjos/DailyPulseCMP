f0d023749cedae510ef395b4a067f1e050393961
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2024-02-27 16:42:43 -0300

    add navigation to iOS app

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,13 +2,22 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
+    @State private var shouldOpenAbout = false
+    
 	var body: some View {
-        ArticlesScreen(viewModel: .init())
-	}
-}
-
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
+        NavigationStack {
+            ArticlesScreen(viewModel: .init())
+                .toolbar {
+                    Button {
+                        shouldOpenAbout = true
+                    } label: {
+                        Label("About", systemImage: "info.circle")
+                            .labelStyle(.titleAndIcon)
+                    }
+                    .popover(isPresented: $shouldOpenAbout) {
+                        AboutScreen()
+                    }
+                }
+        }
 	}
 }
